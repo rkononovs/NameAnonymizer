0f663eea792c11f0e16f37d22ed0a6a48d05a7ef
Romans <50585282+rkononovs@users.noreply.github.com>
2020-11-11 15:03:33 +0200

    Added unique id assigner

--- NameAnonymizer/NameAnonymizer.py
+++ NameAnonymizer/NameAnonymizer.py
@@ -1,34 +1,83 @@
 import nltk
 import os
 import docx
+#import stanza
+from itertools import count
+from collections import defaultdict
+from stanza.server import CoreNLPClient
+
+os.environ["CORENLP_HOME"]="C:/Users/roma0/stanford-corenlp-4.1.0" # Point to the CoreNLP location
+
 
 def preprocess(text):
-    outputFile = "outputFile" + fileExtension
+    outputFile = "outputFile" + fileExtension # Define a output file with extension
 
     ne_person = []
+    ne_person_test = []
     ne_person_list = []
     ne_name = ''
+    id = 0
 
+    # Make Coreference chain which I can use to compare elements in text
+    with CoreNLPClient(
+            annotators=['tokenize','ssplit','pos','lemma','ner', 'parse', 'depparse','coref'],
+            timeout=30000,
+            memory='16G') as client:
+        ann = client.annotate(text)
+
+    mychains = list() # Hold coreference chains
+    chains = ann.corefChain
+    for chain in chains:
+        mychain = list()
+        # Loop through every mention of this chain
+        for mention in chain.mention:
+            # Get the sentence in which this mention is located, and get the words which are part of this mention
+            # (we can have more than one word, for example, a mention can be a pronoun like "he", but also a compound noun like "His wife Michelle")
+            words_list = ann.sentence[mention.sentenceIndex].token[mention.beginIndex:mention.endIndex]
+
+            #build a string out of the words of this mention and filter out only person        
+            ment_word = ' '.join([x.word for x in words_list if x.ner == 'PERSON'])
+
+            # Filter out repeating names and blank words
+            if ment_word != '':
+                if ment_word not in mychain:
+                    mychain.append(ment_word)
+        mychains.append(mychain)
+
+     # Assign unique id to each coreferenc chain
+    mapping = defaultdict(count().__next__)
+    unique_id = []
+    for element in mychains:
+       unique_id.append(mapping[tuple(element)])
+    
+    # Replace all name occurances with a unique tag with identifier
+    temp_id = 0
+    for chain in mychains:
+        for name in chain:
+            unique_name_tag = 'PERSON' + '_' + str(unique_id[temp_id])
+            text = text.replace(name, unique_name_tag)
+        temp_id += 1
+    
+    print(text)
     ne_tree = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(text))) # Chunk, tokenize and label text
-    print(ne_tree)
+    #ne_tree.draw()
 
     for subtree in ne_tree.subtrees(filter=lambda t: t.label() == 'PERSON'): # Filter every instance of the named entity with tag PERSON
-        # print(subtree)
         for leaf in subtree.leaves():
             ne_person.append(leaf[0]) # Append part of full name with title
             # print (ne_person)
         for part in ne_person:
             ne_name += part + ' ' # Add part of persons name to complete it
-            #print(part)
         if ne_name[:-1] not in ne_person_list:
             ne_person_list.append(ne_name[:-1]) # Append full name to person list
+            
             #print (ne_name)
         ne_name = ''
         ne_person = []
         for ne_full_name in ne_person_list: # Replace all full name occurances with a tag 'person' // Change to unique ID
             text = text.replace(ne_full_name, 'PERSON')
 
-    print(ne_person_list) # Print names for DEBUG PURPOSES
+    #print(ne_person_list) # Print names for DEBUG PURPOSES
 
     newPathOut = os.path.relpath('..\\dataSet\\' + outputFile , curPath) # Make new relative path to file
     if fileExtension == ".txt":
@@ -42,7 +91,7 @@ def preprocess(text):
 
 
 text = ""
-fileExtension = ".docx"
+fileExtension = ".docx" #.csv ?.excel?
 curProjectFile = "NameAnonymizer.sln"
 inputFile = "inputFile" + fileExtension
 curPath = os.path.dirname(curProjectFile) # Point to the current project folder
@@ -59,3 +108,5 @@ elif fileExtension == ".docx":
         text = text + paragraph
 
 preprocess(text)
+
+
